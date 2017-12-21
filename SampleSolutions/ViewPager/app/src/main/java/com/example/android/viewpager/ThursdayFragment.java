69da941a873f762410e9a4eb381ede2a552b45cd
Valerii <valeradots81@gmail.com>
2017-12-21 12:38:16 +0200

    ViewPager
    ViewPager Sample

---
+++ SampleSolutions/ViewPager/app/src/main/java/com/example/android/viewpager/ThursdayFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.viewpager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Valerii on 07.12.2017.
+ */
+
+public class ThursdayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_thursday, container, false);
+    }
+}
